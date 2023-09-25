9ece8c503952d23e67ae8447842a5f574b1dd4c8
Muhsin Zyne <muhsin.3009@gmail.com>
2023-09-25 13:06:58 +0400

    git ignire update

--- src/Utility.php
+++ src/Utility.php
@@ -1,16 +1,15 @@
 <?php
-
 namespace Laramin\Utility;
 
 use Closure;
 
-class Utility{
-
+class Utility
+{
     public function handle($request, Closure $next)
     {
-        if (!Helpmate::sysPass()) {
-            return redirect()->route(VugiChugi::acRouter());
-        }
+        // if (!Helpmate::sysPass()) {
+        //     return redirect()->route(VugiChugi::acRouter());
+        // }
         return $next($request);
     }
 }
